0139f7b62601bd353a65905d81a6f5a7640a99d9
praveen s h <66664038+praveensh8970@users.noreply.github.com>
2025-02-09 00:32:48 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,27 @@
+
+def add(num_one, numb_two):
+    """
+    Add two numbers and return the result.
+
+    Parameters:
+    a (int or float): The first number to add.
+    b (int or float): The second number to add.
+
+    Returns:
+    int or float: The sum of the two numbers.
+    """
+    return num_one + numb_two
+
+
+def subtract(num_one, numb_two):
+    """
+    Substract two numbers and return the result.
+
+    Parameters:
+    a (int or float): The first number to substract.
+    b (int or float): The second number to substract.
+
+    Returns:
+    int or float: The substraction of the two numbers.
+    """
+    return num_one - numb_two
